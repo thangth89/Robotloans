52fc0ed613945bd4cc3d3095a11f94fbba4da047
thangth89 <thangth.vinfast@gmail.com>
2025-11-25 23:03:44 +0700

    Update loanSiteConfig.ts

--- lib/loanSiteConfig.ts
+++ lib/loanSiteConfig.ts
@@ -55,7 +55,7 @@ export const faqItems: FaqItem[] = [
   {
     question: "Annual Percentage Rate (APR)",
 answer:
-  "Here at RotboLoans we offer APR rates ranging from 4.95% minimum to 35.95% maximum." +
+  "Here at RobotLoans we offer APR rates ranging from 4.95% minimum to 35.95% maximum." +
   "The APR you may be offered depends on different factors, including your personal situation and financial standing. By completing our loan offer form you shall receive details of the APR and full terms of any loan offered to you." +
   "The actual interest rate you may be offered depends on several factors: your credit score, financial history, income, etc. Contact your lender for more information about the APR you may receive and the terms of your loan agreement.",
   },
@@ -72,22 +72,22 @@ answer:
   {
     question: "Financial Implications (Interest & Finance Charges)",
     answer:
-      "RotboLoans is not a lender and we cannot predict what fees and interest rate will be applied to the loan you will be offered. It is your lender that will provide all the necessary information about the cost of the loan. It is your responsibility to peruse the loan agreement carefully and accept the offer only if you agree to all the terms. Trustedonlineloans service is free of charge, and you are under no obligation to accept the terms that the lender offers you.",
+      "RobotLoans is not a lender and we cannot predict what fees and interest rate will be applied to the loan you will be offered. It is your lender that will provide all the necessary information about the cost of the loan. It is your responsibility to peruse the loan agreement carefully and accept the offer only if you agree to all the terms. RobotLoans service is free of charge, and you are under no obligation to accept the terms that the lender offers you.",
   },
   {
     question: "Implications of Non-Payment",
     answer:
-      "When you accept the terms and conditions for a loan, you enter into a commitment to repay the amount you owe according to the specific schedule outlined in the documents. In the event of non-payment, additional charges may be applied. Late payment penalties vary by lender, and as Trustedonlineloans is not involved in the lending process, we cannot supply any information on the fees you will incur if any default occurs. For more information, please contact the lender directly if you have any issues relating to your loan repayment.",
+      "When you accept the terms and conditions for a loan, you enter into a commitment to repay the amount you owe according to the specific schedule outlined in the documents. In the event of non-payment, additional charges may be applied. Late payment penalties vary by lender, and as RobotLoans is not involved in the lending process, we cannot supply any information on the fees you will incur if any default occurs. For more information, please contact the lender directly if you have any issues relating to your loan repayment.",
   },
   {
     question: "Potential Impact on Credit Score",
     answer:
-      "RotboLoans is not a lender and we do not check your credit standing and credit capacity. When you submit a request at our online platform, this is considered a soft inquiry and does not impact your credit score. However, some lenders may carry out a credit check to ascertain your eligibility for a loan. Most likely they will turn to any of the three major credit bureaus (Transunion, Experian, and Equifax) that is in most cases considered a hard inquiry and may affect your credit score.",
+      "RobotLoans is not a lender and we do not check your credit standing and credit capacity. When you submit a request at our online platform, this is considered a soft inquiry and does not impact your credit score. However, some lenders may carry out a credit check to ascertain your eligibility for a loan. Most likely they will turn to any of the three major credit bureaus (Transunion, Experian, and Equifax) that is in most cases considered a hard inquiry and may affect your credit score.",
   },
   {
     question: "Collection Practices",
     answer:
-      "RotboLoans is not a lender. As such, we are not involved in any debt collection practices and cannot make you aware of any of them. Your lender will specify their collection practices in your loan agreement. If you have any questions regarding the issue, please, address them to your lender. For more information visit our page for Responsible Lending. Trustedonlineloans.com itself isn’t involved in debt collection.",
+      "RobotLoans is not a lender. As such, we are not involved in any debt collection practices and cannot make you aware of any of them. Your lender will specify their collection practices in your loan agreement. If you have any questions regarding the issue, please, address them to your lender. For more information visit our page for Responsible Lending. RobotLoans itself isn’t involved in debt collection.",
   },
   {
     question: "Loan Renewal Policies",
@@ -124,8 +124,9 @@ export const footerConfig = {
     { href: "/rates-and-fees", label: "Rates and Fees" },
     { href: "/responsible-lending", label: "Responsible Lending" },
   ],
-  disclaimer: `RotboLoans is not an offer or solicitation to lend. We do not make any loan or credit decisions and are not representatives, brokers or agents of any lender. Participating lenders offer loans from $200 up to $5,000. Not all lenders can offer you amounts up to $5,000. Our service is not available in all states. Submission of a request through this website does not guarantee that you will receive a loan offer or an offer you'll be satisfied with. Funds transfer time may vary depending on your lender and/or financial institution. Credit checks may be performed with the three reporting credit bureaus: Experian, Equifax, and TransUnion. Submission of a request means you are authorizing the lenders to check your creditworthiness and your personal details. This service is not a solicitation for loan products and does not constitute a loan offer for any loan products that are prohibited by state law. This service is void where prohibited.`,
+  disclaimer: `RobotLoans is not an offer or solicitation to lend. We do not make any loan or credit decisions and are not representatives, brokers or agents of any lender. Participating lenders offer loans from $200 up to $5,000. Not all lenders can offer you amounts up to $5,000. Our service is not available in all states. Submission of a request through this website does not guarantee that you will receive a loan offer or an offer you'll be satisfied with. Funds transfer time may vary depending on your lender and/or financial institution. Credit checks may be performed with the three reporting credit bureaus: Experian, Equifax, and TransUnion. Submission of a request means you are authorizing the lenders to check your creditworthiness and your personal details. This service is not a solicitation for loan products and does not constitute a loan offer for any loan products that are prohibited by state law. This service is void where prohibited.`,
 };
+
 
 
 
