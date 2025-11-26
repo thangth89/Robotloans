a3b0f1afcbe83a086d8ed6e5f6bd634bee58d6d7
thangth89 <thangth.vinfast@gmail.com>
2025-11-26 22:33:03 +0700

    Update page.tsx

--- app/terms-of-use/page.tsx
+++ app/terms-of-use/page.tsx
@@ -3,6 +3,8 @@ import SiteHeader from "../../components/layout/SiteHeader";
 import SiteFooter from "../../components/layout/SiteFooter";
 import HeroSection from "../../components/sections/HeroSection";
 import type { Metadata } from "next";
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   title: "Terms of Use | RobotLoans",
