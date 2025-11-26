8e5dd99522a29adfcfee64515212410fa2a6ec45
thangth89 <thangth.vinfast@gmail.com>
2025-11-26 10:41:59 +0700

    Update page.tsx

--- app/rates-and-fees/page.tsx
+++ app/rates-and-fees/page.tsx
@@ -1,4 +1,7 @@
 // app/rates-and-fees/page.tsx
+import SiteHeader from "../../components/layout/SiteHeader";
+import SiteFooter from "../../components/layout/SiteFooter";
+import HeroSection from "../../components/sections/HeroSection";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
