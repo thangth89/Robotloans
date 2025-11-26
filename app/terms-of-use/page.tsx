82b9009d57202500cca5cb2da253003713e92f6b
thangth89 <thangth.vinfast@gmail.com>
2025-11-26 22:27:59 +0700

    Update page.tsx

--- app/terms-of-use/page.tsx
+++ app/terms-of-use/page.tsx
@@ -12,6 +12,23 @@ export const metadata: Metadata = {
 
 export default function TermsOfUsePage() {
   return (
+        {/* Tracking permate */}
+      <head>
+        <Script
+          id="p-lander-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,r){
+                var i=t.createElement('script'),n=t.scripts[0];
+                i.defer=!0;
+                i.src=r+(-1===r.indexOf('?')?'?':'&')+'time='+(new Date).getTime();
+                n.parentNode.insertBefore(i,n)
+              }(window,document,'https://pmcdn1.com/l.js');
+            `,
+          }}
+        />
+      </head>
 <div className="site-root">
   <SiteHeader />
     <main className="site-main">
