b1743a9c2e761e509cb779d75c7de1edc789b6a5
thangth89 <thangth.vinfast@gmail.com>
2025-11-26 14:53:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,7 +16,28 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="page-body">{children}</body>
+      <body className="page-body">
+        
+      {/* FB Pixel cho toàn site */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', 'YOUR_PIXEL_ID'); // <-- thay YOUR_PIXEL_ID
+              fbq('track', 'PageView');
+            `,
+          }}
+        /> 
+      {children}</body>
     </html>
   );
 }
