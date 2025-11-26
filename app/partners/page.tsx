09c3bd9495d91df9386d051b2d0bd757e0fce404
thangth89 <thangth.vinfast@gmail.com>
2025-11-26 13:45:21 +0700

    Update page.tsx

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -72,34 +72,8 @@ export default function MarketingPracticesPage() {
         <section className="legal-section">
           <h2>Our Marketing Partners</h2>
           <p>
-            RobotLoans works with a wide network of marketing partners and
-            service providers. These partners may help us deliver offers and
-            information that may be relevant to you. The list below is provided
-            for transparency and may change over time.
+            AlumAds, Solution BPO, X1 Info Tech, JG Wentworth, Stop Go Networks, Dot818, Lead Network, Round Sky, Leap Theory, Teapot, ITMEDIA, LeadsMarket, PingLogix, UpAds, Intimate Interactive, Teapot Media, Leadtree Global, Interlincx Media, Freshway Media, Lead Avenue, Affilic Digital, Boomsourcing, Tradition Media Group, Big Step Marketing, Reliable Leads, RevJolt, Complete Home Services, Insurance Guide, National Association of Chiefs of Police, Autistic Children of America, ITP, Infinity One, Quinstreet, Cane Bay, Gulf Coast Leads, Media Blanket, Liftup Capital, UniFi Funding, AAA Auto Title Loans, LLC, CASH 1, LLC (NV), CASH 1 UT, LLC, CASH 1 ID, LLC, CASH 1 KS, LLC, CASH 1 MO, LLC, CASH 1 LA, LLC, ArcaMax Publishing,1st Heritage, 60MonthLoans, AA Media, Accredited Debt Relief, Achieve, Achieve Personal Loans, Acquire Interactive, Acquisition Brands, LLC, Adopt A Contractor, Advance Financial, Advocate Debt Relief, Alleviate Financial Solutions, American Benefits Network, American Express, American Financial support, Americor, Americor Funding, AmOne, Anthem Tax, AppCheckr, ApplicationConnect, Approvana, Argon Credit, Inc., Atlanticus, AutoGravity, Automatic Policy, Avant Credit, Avant, Inc, Avio Credit (Curo Financial), Axcess Financial, Axos Bank, B Two Direct, Best Egg, Better Debt Solutions, Beyond Finance dba Accredited Debt Relief, BHG Financial, Boost Your Score, BorrowersFirst, Inc., Borrowly, Bounce, Bravaro, Braviant Holdings, Brigit, Bulldog Applied Bank Secured Visa Gold Preferred Credit Card, Bulldog Media, Businessloans.com, Campaign Kings, Carbon Steel Media, Caremile, CareOne Debt Relief, Carvana, CASH 1, Cash By Phone, LLC, Cash Central, CashNetUSA, CashUSA, CC Flow - Offered through MoneyKey, CCCS, CDH Association (Credit Debt Health), Century Support Service, CheckGo, Checkgobenefits, CheckgoNOW, Choice Cash, Citi, Citizens Finance, ClearCheck, ClearOne Advantage, Client Consent, Connect Media Ventures, Connectleads (CMVmobile), Consolidated Credit Counseling Services, Consumer Alliance Processing Corp. (CAPC), Consumer Debt Help Association, ConsumerServices, Credible Labs Inc. and its subsidiaries, Credit Central, Credit Ninja, Credit Saint, Credit9, CreditFresh, CreditFresh - Line of Credit from CBW Bank, CreditFresh - Line of Credit from First Electronic Bank, CreditNinja, Current, DailyFinance, Debt Advisor, LLC, Debt Freedom Group, Debt Genie, Debt Wave, Debt.com, DebtMD, Digital Media Solutions, Inc., Digital Thrive, Discover Personal Loan, DMB, Dollar Sensei, DOT818, E-Financy, Echo Credit, eHelpLab, Elevate Credit Service, EMoney, Empower Financial Network, LLC, Engine by MoneyLion, Enova, EPCVIP, Evercare, Fig Loans, Fig Tech, Figure, Finance Factory, FinClear, FinishHub, Fintara, First Midwest Bank, First Nations Media, Fix and Flip, Fluent, Freedom Debt Relief, Freedom Financial, Freedom Lender, Freedom Plus, Fundera, Inc., FundGo, Funding Circle, Fundli, GetBoro, Giggle Finance, Glacier Bay, Global Trust Funding, Go Ace Group, Go-files, GoBlueRate, GoChecks, Goldman Sachs - Marcus, GoSteadyWise LLC, Haven Hub LLC, Headway Capital, Heights Finance, Home Options, Inc., HomesatMV, http://Verfy.co, iLoan, InChare Debt Solutions, INeedCashNow.net, Integra Credit, Interlincx, Jenius Bank, JG Wentworth, Jora Credit, Journey Capital, KC Credit Services, KGM, Kinetiq Media, KleverBudget, Laurel Road, Lead Capture, Lead Economy, LeadPulse Media, LeadsMarket, LeadtreeUSA, Lend A Dollar, LendingClub, LendingPoint LLC, LendingTree Personal Loan Partners, LendingUSA, Lendio, Lendli, Lendly, Lendmark Financial, LendNation, LendSwift, LendUp, Lendvia, Level Debt, Lexington, Lightstream, Loan Depot, Loan Mart - Choice Cash, LoanMe, Loans2Go, LOANZ, Magnum, MakeSaveRetire, Mariner Finance, LLC, Marlette Funding (BestEgg), MaverickPolicy, Medical Sync & Review, Merrick Bank, MiliMedia, MKII, Monevo, MoneyKey, MoneyLion Inc., Mr. Amazing Loans, MV Realty PBC, LLC, My Benefits Club, National Debt Relief, Nestegg, NetCredit, NexLevel Direct, Offer Edge, Oliv Financial, OnDeck, One Loan Place, One Park Financial, OneMain Financial, Ontario Holdings, Opportunity Financial (OppLoans), Opportunity Loans, Ovation, Pacific Debt, Palmcrest (CAPC), PartnerPros, Path56, Pathward, Pave, Payoff, PeachTree Debt Relief, Peerform, Pentagon Federal Credit Union, PersonalLoans.com, Personify Financial, Point Digital Finance, PolicyBright, Possible Finance, PowerLend, Profitise, LLC, Progressive Debt Relief, Progrexion, Prosper Marketplace, Inc, Purpose Financial, QC Holdings, Quick Credit Connect, QuinStreet, QuoteChex, RapidAdvance, Reach Financial LLC, Red Rock Media, Regional Finance, Reprise Financial, Republic Finance LLC, RevLift, RevPie, Rex Direct, Ringlab Media, RISEcredit.com, Rocket Loans, RX Marketing, Sanco (Loan Me), Search ROI, LLC, SecureRequests, Security Finance, SeedFi, Seek Capital, Select Quote Insurance Services, Self Lender, Senior Direct Marketing, Simple Debt Solutions, SimpleQuotes, SimpleVerify, Skeps, SMAC X LLC, Smart Debt Relief, SoFi, Sola, SolveDebts, Solvent, Speedy Cash, Splash Financial, Spot Loan, Stop Go Networks Limited, Super Money, Swift Homes, SymplyNow, Tally, Tandem, Target Audience Group (TAG), ThinkWallet, TitleMax, TMX, ToTheDegrees, Tower Loan, Tradition Media Group (TMG), Transform Credit, TrustedRequest, Trustio, Try My Solutions, Turbo Debt, Turbo Title Loan, TurboTips, Umbrella Debt, Universal Credit, Upgrade, Inc., Upside Legal, LLC, Verify Connect, VIVA Finance, Inc, Vouch Financial, Inc, Willow Wealth Financial, World Finance, Yendo Inc, Zendable, LLC
           </p>
-
-          {/* 
-            Gợi ý: bạn dán full danh sách đối tác của bạn vào đoạn <p> này
-            (hoặc chuyển thành <ul><li>...</li></ul> nếu muốn).
-            Mình để sẵn một ví dụ cấu trúc bên dưới:
-          */}
-          <p className="legal-partners-list">
-            AlumAds, Solution BPO, XI Info Tech, JG Wentworth, Stop Go Networks,
-            Dot818, Lead Network, Round Sky, Leap Theory, Tapcot, ITMEDIA,
-            LeadsMarket, PingLogix, UpAds, Intimate Interactive, Teapot Media,
-            and many other licensed marketing and lead-generation partners.
-          </p>
-
-          {/* Nếu bạn có danh sách rất dài, có thể dùng dạng danh sách: */}
-          {/*
-          <ul className="legal-partners-list">
-            <li>AlumAds</li>
-            <li>Solution BPO</li>
-            <li>XI Info Tech</li>
-            <li>JG Wentworth</li>
-            ...
-          </ul>
-          */}
         </section>
       </div>
     </main>
